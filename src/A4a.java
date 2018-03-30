04d7a04c87d3cc44769b586af635ad43d1d1e5dd
Karl <Karl@RGBeast>
2018-03-30 12:35:16 -0600

    Restart

---
+++ src/A4a.java
@@ -0,0 +1,96 @@
+import java.util.Collections;
+import java.util.Scanner;
+public class A4a {
+	private Scanner in = new Scanner(System.in);
+	private int totalwordcount;
+	private int stopwordcount;
+	
+	private String[] stopwords = { 
+		"a", "about",  "all", "am", "an", "and",  "any", "are", "as", "at", 
+		"be", "been", "but", "by", "can", "cannot", "could", "did", "do", "does", 
+		"else", "for", "from", "get", "got", "had", "has", "have", "he", "her", 	
+		"hers", "him", "his", "how", "i", "if", "in", "into", "is", "it", 
+		"its", "like", "more", "me", "my", "no", "now", "not", "of", "on", 
+		"one", "or", "our", "out", "said", "say", "says", "she", "so", "some",
+		"than", "that", "thats", "the", "their", "them", "then", "there", "these", "they", "this", 
+		"to", "too", "us", "upon", "was", "we", "were", "what", "with", "when", 
+		"where", "which", "while", "who", "whom", "why", "will", "you", "your", "up", "down", "left", "right",
+		"man", "woman", "would", "should", "dont", "after", "before", "im", "men"
+   };
+	
+	public static void main(String args[]) 
+	{
+		A4a a4 = new A4a();
+		a4.run();
+	}
+	
+	public void run() 
+	{
+		read();
+	}
+	
+	public void read() 
+	{
+		BST<Word> words = new BST<Word>();
+		for(int i = 0; i < stopwords.length; i++) {
+			Word s = new Word(stopwords[i]);
+			words.add(s);
+		}
+		
+		System.out.println("Hit");
+		while(in.hasNext()) {
+	        String word = in.next().toLowerCase().trim().replaceAll( "[^a-z]","");
+	        if(word.length() > 0) {
+		        totalwordcount++;
+		        Word w = new Word(word);
+			    words.add(w);
+			    System.out.println(words.size());
+	        }
+	    }
+		
+		for(int i = 0; i < stopwords.length; i++) {
+			Word s = new Word(stopwords[i]);
+			words.deleteData(s);
+		}
+		
+		in.close();
+		
+		for(int i = 0; i < stopwords.length; i++) {
+			Word s = new Word(stopwords[i]);
+			words.deleteData(s);
+		}
+	}
+	
+	   /** 
+	   List the first n words in the list of words.
+	   @param n the number of words to list
+	   */
+	   private void printWords( int n) 
+	   {
+	      int i = 0;
+	      while ( i < words.size() && i < n) 
+	      {
+	         System.out.println( words.get(i));
+	         i++;
+	      }
+	   }
+	
+	  private void printResults() 
+	   {
+	       System.out.println( "Total Words: " + totalwordcount);
+	       System.out.println( "Unique Words: " + words.size()); 
+	       System.out.println( "Stop Words: " + stopwordcount);
+	       System.out.println();
+	       System.out.println( "10 Most Frequent");
+	       Collections.sort( words, Word.CompFreqDesc); 
+	       printWords(10); 
+	       System.out.println();
+	       System.out.println( "10 Least Frequent");
+	       Collections.sort( words, Word.CompFreqAsc); 
+	       printWords(10);
+	       System.out.println();
+	       System.out.println( "All");
+	       Collections.sort( words); 
+	       printWords( words.size());
+	   }
+}
---
+++ src/BST.java
@@ -0,0 +1,288 @@
+import java.util.Iterator;
+public class BST<T extends Comparable<T>> 
+{
+    class BSTNode implements Comparable<BSTNode>
+    {
+        private T data;
+        private BSTNode left; 
+        private BSTNode right; 
+
+        public BSTNode( T d) 
+        {
+            setLeft( null);
+            setRight( null);
+            setData( d);
+        }
+
+        public T getData() { return data; }
+
+        public void setData( T d) { data = d; }
+
+        public void setLeft( BSTNode l) { left = l;}
+
+        public void setRight( BSTNode r) { right = r;}
+
+        public BSTNode getLeft()  { return left;}
+
+        public BSTNode getRight()  { return right;}
+
+        public boolean isLeaf() 
+        { 
+            return ( getLeft() == null) && ( getRight() == null);
+        }
+
+        public int compareTo( BSTNode o) 
+        {
+            return this.getData().compareTo( o.getData());
+        }
+
+    }
+
+    // The different traversal types. 
+    public static final int INORDER = 0;
+    public static final int PREORDER = 1;
+    public static final int POSTORDER = 2;
+    public static final int LEVELORDER = 3;
+
+    private BSTNode root;
+    private int size;  
+
+    public BST()
+    {
+        root = null;
+        size = 0;
+    }
+
+    /** Return true if element d is present in the tree. 
+     */
+    public T find( T d) 
+    {
+        return find( d, root); 
+    }
+
+    /** Return the height of the tree. 
+     */
+    public int height() 
+    {
+        return height( root);
+    }
+
+    /** Return the number of nodes in the tree. 
+     */
+    public int size() { 
+        return size;
+    }
+
+    public void inOrder() 
+    {
+        traverse( root, INORDER);
+    }
+
+    public void postOrder() 
+    {
+        traverse( root, POSTORDER);
+    }
+
+    public void preOrder() 
+    {
+        traverse( root, PREORDER);
+    }
+
+    public void levelOrder() 
+    {
+        traverse( root, LEVELORDER);
+    }
+
+    // Private methods. 
+    private T find( T d, BSTNode r) 
+    {
+        if ( r == null) 
+            return null;
+        int c = d.compareTo( r.getData()); 
+        if ( c == 0) 
+            return r.getData();
+        else if ( c < 0) 
+            return find( d, r.getLeft());
+        else 
+            return find( d, r.getRight());
+    } 
+
+    /** Add element d to the tree. 
+     */
+    public void add( T d) 
+    {
+        BSTNode n = new BSTNode( d); 
+        if ( root == null) 
+        {
+            size++;
+            root = n; 
+        }
+        else 
+        {
+            add( root, n); 
+        }
+    }
+
+    /* Do the actual add of node r to tree rooted at n */
+    private void add( BSTNode r, BSTNode n) 
+    {
+        int c = n.compareTo( r);
+        if ( c < 0) //if n less than  r
+        {
+            if(r.getLeft() == null)
+            {
+                r.setLeft(n);
+                size++;
+            }
+            else
+            {
+                add(r.getLeft(), n);
+            }
+        }
+        else //if n greater than or equal to r (allows duplicate enteries)
+        {
+            if(r.getRight() == null)
+            {
+                r.setRight(n);
+                size++;
+            }
+            else
+            {
+                add(r.getRight(), n);
+            }
+        }
+    }
+
+    /* Implement a height method. */
+    private int height( BSTNode r) 
+    {
+        int h = 0;
+
+        if (r != null)
+
+        {
+            int rh = height(r.getRight());
+            int lh = height(r.getLeft());
+
+            h =(rh > lh ? 1+ rh : 1 +lh);
+
+        }
+        return h;
+    } 
+
+    /* Traverse the tree.  travtype determines the type of 
+    traversal to perform. */
+    private void traverse( BSTNode r, int travType) 
+    {
+        if ( r != null) 
+        {
+            switch ( travType) 
+            {
+                case INORDER: 
+                traverse(r.getLeft(), travType);
+                visit(r);
+                traverse(r.getRight(), travType);
+                break;
+
+                case PREORDER: 
+                visit(r);
+                traverse(r.getLeft(), travType);
+                traverse(r.getRight(), travType);
+                break;
+
+                case POSTORDER: 
+                traverse(r.getLeft(), travType);
+                traverse(r.getRight(), travType);
+                visit(r);
+                break;
+                
+                case LEVELORDER:
+                levelOrder( r);
+                break;
+            }
+        }
+    }
+
+    private void visit( BSTNode r) 
+    {
+        if ( r != null) System.out.println( r.getData());
+    }
+
+    // traverse the subtree r using level order. */
+    private void levelOrder( BSTNode r) 
+    {
+        BSTNode currNode = r;
+        Queue<BSTNode> q = new Queue<BSTNode>();
+        q.enqueue(currNode);
+        
+        while(!q.isEmpty())
+        {
+            currNode = q.dequeue();
+            visit(currNode);
+            if(currNode.getLeft() != null){
+                q.enqueue(currNode.getLeft());
+            }
+            if(currNode.getRight() != null){
+                q.enqueue(currNode.getRight());
+            }
+        }
+
+    }
+    
+    // This method mainly calls deleteRec()
+    public void deleteData(T key)
+    {
+        root = deleteRec(root, key);
+        size--;
+    }
+ 
+    /* A recursive function to insert a new key in BST */
+    public BSTNode deleteRec(BSTNode root, T key)
+    {
+        /* Base Case: If the tree is empty */
+        if (root == null)  return root;
+ 
+        /* Otherwise, recur down the tree */
+        int val = key.compareTo(root.getData());
+        if (val < 0) {
+            root.left = deleteRec(root.left, key);
+        }
+        else if (val > 0) {
+            root.right = deleteRec(root.right, key);
+        }
+        
+ 
+        // if key is same as root's key, then This is the node
+        // to be deleted
+        else
+        {
+            // node with only one child or no child
+            if (root.left == null) {
+            		return root.right;
+            	}
+            	
+            else if (root.right == null) {
+            		return root.left;
+            }
+ 
+            // node with two children: Get the inorder successor (smallest
+            // in the right subtree)
+            root.setData(minValue(root.right));
+            
+            // Delete the inorder successor
+            root.right = deleteRec(root.right, root.getData());
+        }
+        return root;
+    }
+ 
+    public T minValue(BSTNode r)
+    {
+        T minv = r.getData();
+        while (r.left != null)
+        {
+            minv = r.left.getData();
+            root = r.left;
+        }
+        return minv;
+    }
+}
